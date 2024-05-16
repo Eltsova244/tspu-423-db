e512f8ab66f76ae13861b26bb58d3f931b048fde
Eltsova244 <124855513+Eltsova244@users.noreply.github.com>
2024-05-16 15:22:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 import sqlite3
 
-# Создаем подключение к базе данных (файл my_database.db будет создан)
+# Устанавливаем соединение с базой данных
 connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
 
+# Создаем таблицу Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id - INT(11) NOT NULL AUTO_INCREMENT
+Фамилия - VARCHAR(60) NOT NULL
+Имя - VARCHAR(30) NOT NULL
+Отчество - VARCHAR(30)
+Дата рождения - DATE NOT NULL
+Email - VARCHAR(50) NOT NULL
+Телефон - CHAR(20)
+Форма обучения - ENUM("очная", "заочная", "очно-заочная") NOT NULL
+Статус - TINYINT(1) NOT NULL DEFAULT = 1
+Пароль - VARCHAR(32) NOT NULL
+ID_факультета - INT(11) NOT NULL
+ID_группы - INT(11) NOT NULL
+
+)
+''')
+
+# Создаем таблицу Faculties
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Faculty (
+id - INT(11) NOT NULL AUTO_INCREMENT
+Название - VARCHAR(100) NOT NULL
+
+
+)
+''')
+
+# Создаем таблицу Groups
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Groups (
+id - INT(11) NOT NULL AUTO_INCREMENT
+Название - VARCHAR(100) NOT NULL
+ID_факультета - INT(11) NOT NULL
+
+)
+''')
+# Сохраняем изменения и закрываем соединение
+connection.commit()
 connection.close()
