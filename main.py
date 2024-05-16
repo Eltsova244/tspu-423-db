0bf7be5806e64cff0848492076115fc053eb5fac
Eltsova244 <124855513+Eltsova244@users.noreply.github.com>
2024-05-16 16:27:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ cursor = connection.cursor()
 # Создаем таблицу Users
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users (
-id - INT(11) NOT NULL AUTO_INCREMENT
-Фамилия - VARCHAR(60) NOT NULL
-Имя - VARCHAR(30) NOT NULL
-Отчество - VARCHAR(30)
-Дата рождения - DATE NOT NULL
-Email - VARCHAR(50) NOT NULL
-Телефон - CHAR(20)
+id INT(11) NOT NULL AUTO_INCREMENT
+Фамилия VARCHAR(60) NOT NULL
+Имя VARCHAR(30) NOT NULL
+Отчество VARCHAR(30)
+Дата рождения DATE NOT NULL
+Email VARCHAR(50) NOT NULL
+Телефон CHAR(20)
 Форма обучения - ENUM("очная", "заочная", "очно-заочная") NOT NULL
-Статус - TINYINT(1) NOT NULL DEFAULT = 1
-Пароль - VARCHAR(32) NOT NULL
-ID_факультета - INT(11) NOT NULL
-ID_группы - INT(11) NOT NULL
+Статус TINYINT(1) NOT NULL DEFAULT = 1
+Пароль VARCHAR(32) NOT NULL
+ID_факультета INT(11) NOT NULL
+ID_группы INT(11) NOT NULL
 
 )
 ''')
